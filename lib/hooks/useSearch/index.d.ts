273332208ac94553176817f26f93b7938016a17e
孟天任 <537059814@qq.com>
2025-05-13 17:54:53 +0800

    fix: 可视化

--- lib/hooks/useSearch/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Component } from 'vue';
-import { IField, ISearchExpose } from '../../components/baseTablePage/table-page';
-export declare const useSearch: (options: IField) => [Component, ISearchExpose];
